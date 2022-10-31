95cf8c41dec189901e80507ea1122c21f3f42408
Daniel Król <danielkrol90@gmail.com>
2022-10-31 19:49:53 +0100

    creating loading and error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Error = () => (
-  <div>Error</div>
+  <div className="w-full flex justify-center items-center flex-col">
+    <h1 className="font-bold text-2xl text-white mt-2">
+      OH NO! You found our SECRET! But We won&apos;t show you it.
+      Just some error here, try again. Try refresh page.
+    </h1>
+  </div>
 );
 
 export default Error;
